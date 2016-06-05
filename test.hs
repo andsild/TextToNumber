a8d150747e99165b6b819c20d4d18054bb14af40
asildnes <andsild@gmail.com>
2016-06-05 23:41:54 +0200

    If life is a stage, I want some better lighting.

--- test.hs
+++ test.hs
@@ -19,7 +19,7 @@ data Unop = Not deriving Show
 data Join = And deriving Show
 data Duop = Band | Iff deriving Show
 data Count = Number Integer | Unit String deriving Show
-data Stmt = Seq [Stmt] | UnitNumber Count Stmt 
+data Stmt = Seq [Stmt] | UnitNumber Count Stmt | Join Stmt
           deriving Show
 
 unitnumbers :: [String]
@@ -30,17 +30,16 @@ elemIndex' s = toInteger $ fromMaybe 0 $ elemIndex s unitnumbers
 
 
 def :: LanguageDef st
-def = emptyDef{ commentStart = "{-"
-              , commentEnd = "-}"
-              , identStart =  alphaNum
+def = emptyDef{ identStart =  alphaNum
               , identLetter = alphaNum
               , opStart = oneOf "-"
               , opLetter = oneOf "-"
               , reservedOpNames = ["and"]
               , reservedNames = []
               }
-TokenParser{ parens = m_parens
-           , identifier = m_identifier
+
+TokenParser{ 
+           identifier = m_identifier 
            , reservedOp = m_reservedOp
            , reserved = m_reserved
            , natural = m_natural
@@ -54,17 +53,10 @@ table = [ [Prefix (m_reservedOp "~" >> return (Uno Not))]
         , [Infix (m_reservedOp "=" >> return (Duo Iff)) AssocLeft]
         , [Infix (m_reservedOp "and" >> return (Duo Iff)) AssocLeft]
         ]
-term = m_parens exprparser
-       <|> fmap Var m_identifier
-       -- <|> fmap Natural m_natural
-       -- <|> (NaturalJoin m_natural m_natural  >> return (NaturalJoin ))
-       <|> (m_reserved "true" >> return (Con True))
-       <|> (m_reserved "false" >> return (Con False))
+term = fmap Var m_identifier
 
 returnToken :: Integer -> Stmt -> Stmt
 returnToken num = UnitNumber (Number num)
-  -- | num > 10 = Number num
-  -- | otherwise = SmallerNumber num
 
 convert :: String -> Stmt -> Stmt
 convert s = UnitNumber (Unit s)
@@ -82,27 +74,26 @@ mainparser = m_whiteSpace >> stmtparser <* eof
                        ; e <- stmtparser
                     ; return (convert num e)
                     }
-
-takeN :: Integer -> [a] -> [a]
-takeN n l = take (fromIntegral n) l
+              <|> do { m_reservedOp "and"
+                     ; e <- stmtparser
+                     ; return (Join e)
+              }
 
 pairwise :: [Int] -> [(Int, Int)]
 pairwise xs = zip (0 : xs) xs
 
 valueOf :: Stmt -> Integer
 valueOf stmt = case stmt of
+  (Seq a) -> sum $ parseFuck a
+  (Join a) -> valueOf a
   (UnitNumber (Number num) (Seq a)) -> num + sum (parseFuck a)
-  (UnitNumber (Unit num) (Seq a)) -> elemIndex' num 
-
-recursiveStmtParser [] = 0
-recursiveStmtParser [x] = 0
-recursiveStmtParser (x:xs) = 0
+  (UnitNumber (Unit num) (Seq a)) -> elemIndex' num  + sum (parseFuck a)
 
 
 parseFuck :: [Stmt] -> [Integer]
 parseFuck [] = []
 parseFuck [x] = [valueOf x]
-parseFuck (x:xs) = [valueOf x] ++ parseFuck xs
+parseFuck (x:xs) = valueOf x : parseFuck xs
 
 interpreter :: Stmt -> Integer
 interpreter (Seq a) = sum (parseFuck a)
